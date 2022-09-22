8c7343d5c3aac343efffffee6747a9379def8368
jim <jimh@datagrove.com>
2022-09-22 00:14:40 -0400

    init

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+import { EditorState } from "prosemirror-state"
+import { EditorView } from "prosemirror-view"
+import { Schema, DOMParser } from "prosemirror-model"
+import { schema } from "prosemirror-schema-basic"
+import { addListNodes } from "prosemirror-schema-list"
+import { exampleSetup } from "prosemirror-example-setup"
+
+// Mix the nodes from prosemirror-schema-list into the basic schema to
+// create a schema with list support.
+const mySchema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  marks: schema.spec.marks
+});
+
+(window as any).view = new EditorView(document.querySelector("#editor"), {
+  state: EditorState.create({
+    doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")!),
+    plugins: exampleSetup({ schema: mySchema })
+  })
+})
